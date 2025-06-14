72f0b6107e7e0d9d424aac53ab31bd068ac69e56
Mauricio Andres Alfonso Forero <mauro311095@icloud.com>
2025-06-15 07:50:54 +1000

    feat(html): add recovery password form in login.html

--- static/script.js
+++ static/script.js
@@ -1,12 +1,41 @@
 var loginForm = document.getElementById('login');
 var registerForm = document.getElementById('register');
+var recoverForm = document.getElementById('recover');
 
 function login() {
     loginForm.style.left = '4px';
     registerForm.style.right = '-520px';
+    recoverForm.style.top = '-520px';
+
+    loginForm.style.zIndex = 2;
+    registerForm.style.zIndex = 1;
+    recoverForm.style.zIndex = 0;
 }
 
 function register() {
     loginForm.style.left = '-520px';
     registerForm.style.right = '4px';
+    recoverForm.style.top = '-520px';
+
+    loginForm.style.zIndex = 1;
+    registerForm.style.zIndex = 2;
+    recoverForm.style.zIndex = 0;
+}
+
+function recover() {
+    loginForm.style.top = '-520px';
+    recoverForm.style.top = '4px';
+
+    loginForm.style.zIndex = 1;
+    registerForm.style.zIndex = 0;
+    recoverForm.style.zIndex = 2;
+}
+
+function recoverLogin() {
+    loginForm.style.top = '4px';
+    recoverForm.style.top = '-520px';
+
+    loginForm.style.zIndex = 2;
+    registerForm.style.zIndex = 0;
+    recoverForm.style.zIndex = 1;
 }
